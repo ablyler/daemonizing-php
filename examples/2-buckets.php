0387229e7488b889e192e4d8b13fa93fcc3ac88e
Andy Blyler <andy@blyler.cc>
2013-07-19 23:57:27 -0400

    fix syntax in examples/2-buckets.php

--- examples/2-buckets.php
+++ examples/2-buckets.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../fork_daemon.php');
+require_once(__DIR__ . '/../modules/forkdaemon-php/fork_daemon.php');
 
 /* setup forking daemon */
 $server = new fork_daemon();
@@ -8,7 +8,6 @@ $server->max_children_set(5);
 $server->max_work_per_child_set(3);
 $server->register_child_run("process_child_run");
 $server->register_parent_child_exit("process_child_exit");
-$server->register_logging("logger", fork_daemon::LOG_LEVEL_ALL);
 
 test_bucket();
 
@@ -27,7 +26,7 @@ function test_bucket()
 	$server->register_child_run("process_child_run_2", BUCKET2);
 
 	$data_set = array();
-	for($i=0; $i<100; $i++) $data_set[] = $i;
+	for($i=0; $i<10; $i++) $data_set[] = $i;
 
 	/* add work to bucket 1 */
 	shuffle($data_set);
@@ -69,10 +68,4 @@ function process_child_run($data_set, $identifier = "")
 function process_child_exit($pid, $identifier = "")
 {
 	echo "Child $pid just finished" . ($identifier == "" ? "" : " (id:$identifier)") . "\n";
-}
-
-/* registered call back function */
-function logger($message)
-{
-	echo "logger: " . $message . PHP_EOL;
 }
